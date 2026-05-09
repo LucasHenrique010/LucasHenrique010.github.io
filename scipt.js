acb80c400abfc0d22b01d2635aabc9572d441586
Lucas Henrique <lh2767196@icloud.com>
2026-05-08 21:29:20 -0300

    Atualização

--- scipt.js
+++ scipt.js
@@ -1,3 +1,40 @@
-function clicou() {
-  alert("Você clicou! 🎉");
+function avancar(){
+
+  const nome =
+    document.getElementById("nome").value
+
+  const telefone =
+    document.getElementById("telefone").value
+
+  const endereco =
+    document.getElementById("endereco").value
+
+  const cpf =
+    document.getElementById("cpf").value
+
+  if(
+    nome === "" ||
+    telefone === "" ||
+    endereco === "" ||
+    cpf === ""
+  ){
+
+    alert("Preencha todos os campos!")
+
+    return
+  }
+
+  localStorage.setItem("nome", nome)
+  localStorage.setItem("telefone", telefone)
+  localStorage.setItem("endereco", endereco)
+  localStorage.setItem("cpf", cpf)
+
+  window.location.href = "crianca.html"
+}
+
+function finalizar(){
+
+  alert(
+    "Cadastro realizado com sucesso! 🎉"
+  )
 }
